22232a102b8dd488f3a9eda3f6b93a69665be699
Eliezer Kanal <eykanal@erikdev.com>
2014-05-13 23:33:58 -0400

    added functionality for multiple workouts, working, still WIP

--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,7 @@
 function exerciseTimer(exercises) {
 	
-	var time = document.getElementById('time');
-	var desc = document.getElementById('desc');
+	var time = document.getElementById("time");
+	var desc = document.getElementById("desc");
 	
 	var exercise = exercises.shift();
 	desc.textContent = exercise[0];
@@ -12,7 +12,7 @@ function exerciseTimer(exercises) {
 		desc.textContent = exercise[0];
 		time.textContent = exercise[1];
 		
-		document.getElementById('time').textContent = exercise[1].toFixed(0);
+		document.getElementById("time").textContent = exercise[1].toFixed(0);
 		exercise[1] = exercise[1] - 1;
 		
 		if (exercise[1] <= 0) { 
@@ -28,5 +28,19 @@ function exerciseTimer(exercises) {
 }
 
 function showExercise(exercises) {
+	var child = [];
+	while(exercise = exercises.shift()) {
+		l = child.push($("<table class='table'></table>")) - 1;  // "-1" at end to convert count into index
+		child[l].attr("name", JSON.stringify(exercise));
 
+		$("<thead><tr><th>Exercise</th><th>Time</th></tr></thead>").appendTo(child[l]);
+		exercise.workout.forEach(function(e, i, a) {
+			$("<tr><td>"+a[i][0]+"</td><td>"+a[i][1]+"</td></tr>").appendTo(child[l]);
+		})
+	}
+
+	child.forEach(function(e, i, a) {
+		var h = $("<div class='col-md-4'></div>").append(e);
+		$("#workouts").append(h);
+	})
 }
